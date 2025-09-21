303bf275ed7440cc5ad77ec3d2e1c740bba97eed
Nathan Ringo <me@remexre.com>
2025-09-21 01:59:03 -0500

    Update generate_bootstub for memory management changes.
    Signed-off-by: Nathan Ringo <me@remexre.com>

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -17,16 +17,15 @@
 [[noreturn]]
 void main(u64 hart_id, paddr devicetree_start, paddr kernel_start,
           paddr kernel_end, const struct Elf64_Sym *symtab, usize symtab_len,
-          const char *strtab, usize strtab_len, uptr free_va_start,
-          uptr free_va_end) {
+          const char *strtab, usize strtab_len) {
   print("Starting to boot ukoOS...");
   run_selftests();
   init_boothart_hart_locals(hart_id);
   entropy_pool_init();
   symbolicate_init(symtab, symtab_len, strtab, strtab_len);
   devicetree_init(devicetree_start);
-  devicetree_mm_init(kernel_start, kernel_end, &free_va_start, &free_va_end);
-  mm_init_virtual(free_va_start, free_va_end);
+  // devicetree_mm_init(kernel_start, kernel_end, &free_va_start, &free_va_end);
+  // mm_init_virtual(free_va_start, free_va_end);
 
   uptr a = mm_va_alloc(mm_kernel_virtual_buddy, 2 * 1024 * 1024);
   uptr b = mm_va_alloc(mm_kernel_virtual_buddy, 4096);
