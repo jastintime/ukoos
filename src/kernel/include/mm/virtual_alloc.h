e64280c96bddc92eb91bd9a51c26af6b67eb852e
Nathan Ringo <me@remexre.com>
2025-09-28 16:21:22 -0500

    Data structure definitions for the new virtual allocator.
    Signed-off-by: Nathan Ringo <me@remexre.com>

--- src/kernel/include/mm/virtual_alloc.h
+++ src/kernel/include/mm/virtual_alloc.h
@@ -7,74 +7,29 @@
 #ifndef UKO_OS_KERNEL__MM_VIRTUAL_ALLOC_H
 #define UKO_OS_KERNEL__MM_VIRTUAL_ALLOC_H 1
 
-#include <types.h>
+#include <list.h>
 
 /**
- * Flags about a link of the free list.
+ * A virtual memory area. This struct represents a region of either free or
+ * allocated memory, with page granularity or greater.
  */
-enum virtual_buddy_free_list_flags : u16 {
-  /**
-   * This link was allocated in static memory as part of bootstrapping the
-   * allocator, and should not be freed.
-   */
-  VIRTUAL_BUDDY_FREE_LIST_BOOTSTRAP = 1 << 0,
-};
+struct vma;
 
 /**
- * A link in the free list for the buddy allocator for virtual address space.
- * These are heap-allocated.
+ * An instance of the virtual memory allocator.
  */
-struct virtual_buddy_free_list {
-  /**
-   * The next link in the list.
-   */
-  struct virtual_buddy_free_list *next;
-
-  /**
-   * Flags about this link.
-   */
-  enum virtual_buddy_free_list_flags flags : 12;
-
-  /**
-   * The high bits of the address of the allocatable block of memory.
-   */
-  uptr addr_hi_bits : (sizeof(uptr) * 8) - 12;
-};
-
-static_assert(sizeof(struct virtual_buddy_free_list) == 2 * sizeof(uptr));
-static_assert(alignof(struct virtual_buddy_free_list) == alignof(uptr));
+struct vma_allocator;
 
 /**
- * A buddy allocator for a 26-bit space. If pages are the leaves, this covers
- * 256GiB, i.e. either the lower or upper half of Sv39 (and still a convenient
- * size on x86_64, or on ARMv8-A with a 4KiB granule).
+ * Creates a new instance of the virtual memory allocator, set to cover the
+ * given address range.
  */
-struct virtual_buddy {
-  /**
-   * The range of virtual addresses this allocator covers.
-   */
-  uaddr start, end;
-
-  /**
-   * The free lists.
-   */
-  struct virtual_buddy_free_list *free_lists[27];
-
-  /**
-   * The bitmap.
-   */
-  u8 bitmap[1 << 25];
-};
+struct vma_allocator *vma_allocator_new(uaddr lo, uaddr hi);
 
 /**
- * The kernel's virtual memory allocator.
+ * Prints the VMA allocator's state, for debugging purposes.
  */
-extern struct virtual_buddy *const mm_kernel_virtual_buddy;
-
-/**
- * Sets up `mm_kernel_virtual_buddy`.
- */
-void mm_init_virtual(uptr free_va_start, uptr free_va_end);
+void vma_allocator_print(struct vma_allocator *allocator);
 
 /**
  * Allocates a range of virtual address space from the given allocator.
